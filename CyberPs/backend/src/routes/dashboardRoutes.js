ba8dbdc048cd219269eda5c5bef9051864e4de0a
SRH STOCKS <56990617+publicusevs@users.noreply.github.com>
2026-05-10 22:38:01 +0530

    Delete CyberPs directory

--- CyberPs/backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const { authenticate } = require('../middleware/authMiddleware');
-
-router.get('/', authenticate, dashboardController.getStats);
-
-module.exports = router;
